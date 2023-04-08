49b7dad9daf4b88a8f68a2bd0a354bf625f50f94
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-08 22:19:25 +0500

    Initial Commit

---
+++ NavigationViewSwiftUI/NavigationViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewSwiftUIApp.swift
+//  NavigationViewSwiftUI
+//
+//  Created by sss on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
