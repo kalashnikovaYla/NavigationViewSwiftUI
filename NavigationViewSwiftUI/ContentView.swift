c32e7fd69392fec10072effa8a653f541e782484
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 01:34:41 +0500

    Initial

--- NavigationViewSwiftUI/ContentView.swift
+++ NavigationViewSwiftUI/ContentView.swift
@@ -7,15 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DetailView: View {
+    
+    @Environment(\.dismiss) var presentation
+    @EnvironmentObject var userBuy: UserBuy
+    
+    var text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Buy \(text) \(userBuy.caps)")
+            
+                
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            presentation()
+                        } label: {
+                            Text("Back")
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack {
+                            Button {
+                                userBuy.caps += 1
+                            } label: {
+                                Text("+")
+                            }
+                            
+                            Button {
+                                guard userBuy.caps > 0 else {return}
+                                userBuy.caps -= 1
+                            } label: {
+                                Text("-")
+                            }
+                        }
+                    }
+                    
+                })
+            .navigationBarBackButtonHidden()
         }
-        .padding()
+        
+        .onAppear() {
+            userBuy.caps = 0
+        }
+    }
+        
+}
+
+class UserBuy: ObservableObject {
+    @Published var caps = 0
+}
+
+
+struct ContentView: View {
+    
+    @ObservedObject var userBuy = UserBuy()
+    @State var isShow = false
+    
+    let coffee = "Coffee"
+    let tea = "Tea"
+    
+    var body: some View {
+        NavigationStack {
+            
+            VStack(spacing: 30) {
+                Text("What would you like to order?")
+                Text("You choose \(userBuy.caps) caps")
+                
+               
+                NavigationLink(isActive: $isShow) {
+                    DetailView(text: tea)
+                } label: {
+                    EmptyView()
+                }
+                
+                Button {
+                    isShow = true
+                } label: {
+                    Text("Go")
+                }
+
+            }
+            
+            .navigationBarTitle("Menu", displayMode: .large)
+        }
+        .environmentObject(userBuy)
     }
 }
 
